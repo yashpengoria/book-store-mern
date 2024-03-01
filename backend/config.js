8334f638b6022fa858e22d3d05d22daf76be8544
yashpengoria <yashpengoria2001@gmail.com>
2024-03-02 00:10:43 +0530

    Update .gitignore and backend config for improved security

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
+import URL from ".env"
 export const PORT = 5555;
 
-export const mongoDBURL=
-"mongodb+srv://yashpengoria0505:BYNpp0360G@book-store-mern.cqxmkeu.mongodb.net/books-collection?retryWrites=true&w=majority";
+export const mongoDBURL= URL
